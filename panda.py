f2e83ead5bcc1fe15f9f515cedd20109e845d0ce
SounteKamal <Kamalpreetsingh2408@gmail.com>
2025-07-22 14:32:38 +0530

    Practice Day 2

---
+++ panda.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+# data = [10,20,30,40,50]
+# series = pd.Series(data)
+# print(series)
+
+# marks = pd.Series([85, 90, 75], index = ["Math", "Science","English" ])
+# print(marks)
+# print(marks["Math"])
+
+# print(marks + 5)
+# print((marks > 80))
+
+# fruits = pd.Series([50,25,60], index=["Apple", "Banana", "Mango"])
+# print(fruits["Mango"])
+# print(fruits + 10)
+
+# data = {
+#     "Name": ["Kamal", "Karan", "Vikas"],
+#     "Age": [25, 30, 35],
+#     "City": ["Aulakh", "Amritsar", "Batala"]
+# }
+
+# df = pd.DataFrame(data)
+# print(df)
+# print(df["Name"])
+# print(df.loc[0])  # Accessing the first row 
+# print(df.iloc[1])  # Accessing the second row by index
+# print(df.iloc[0:2,:2])  # Accessing the first two rows
+# df["Country"] = ["India", "India", "India"]  # Adding a new column
+# print(df)
+
+# print(df.shape)
+# print(df.describe())  # Summary statistics of the DataFrame
+# print(df.info())# Information about the DataFrame
+
+# data = {
+#     "Student": ["Kamal", "Karan", "Vikas"],
+#     "Marks": [25, 30, 35],
+#     "Subject": ["Science","Science","Science"],}
+# df = pd.DataFrame(data)
+# print(df)
+
+# df["Grade"] = ["C", "B", "A"]  # Adding a new column
+# print(df)
+
+# print(df["Marks"])
+
+# print(df.iloc[1])
+
